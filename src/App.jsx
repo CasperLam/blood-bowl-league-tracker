52da6d9de14f31d7f929fa61d7de9b27eb164851
Casper Lam <casper.lam.kh@googlemail.com>
2024-04-07 21:21:09 +0100

    add My Leagues page and subheader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./app.scss";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
+import MyLeagues from "./pages/myLeagues/myLeagues";
 import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<MyLeagues />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/pages/myLeagues/myLeagues.jsx
@@ -0,0 +1,12 @@
+import "./myLeagues.scss";
+
+export default function MyLeagues() {
+  return (
+    <>
+      <section className="subheader">
+        <h2 className="subheader__text">My Leagues</h2>
+        <button className="subheader__btn">+ Add League</button>
+      </section>
+    </>
+  );
+}
