a8897782d4c3934db4e2a93774986deeaf605a7a
Casper Lam <casper.lam.kh@googlemail.com>
2024-04-08 15:06:25 +0100

    get league data from database

--- src/components/leagueCard/leagueCard.jsx
+++ src/components/leagueCard/leagueCard.jsx
@@ -1,19 +1,43 @@
 import edit_icon from "../../assets/icons/edit_icon.svg";
 import delete_icon from "../../assets/icons/delete_icon.svg";
 import "./leagueCard.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function LeagueCard({ name, date, teams }) {
+export default function LeagueCard({ id, name, date }) {
+  const apiURL = `http://localhost:5050`;
+  const user_id = 1;
+
+  const [teamCount, setTeamCount] = useState(0);
+
+  const getTeamCount = async () => {
+    try {
+      const { data } = await axios.get(
+        `${apiURL}/api/leagues/${user_id}/${id}`
+      );
+      setTeamCount(data.length);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTeamCount();
+  }, []);
+
   return (
     <article className="leagues__card">
-      <h3 className="leagues__name">Name</h3>
+      <h3 className="leagues__name">{name}</h3>
+      <div className="leagues__buttons">
+        <img src={edit_icon} alt="edit icon" className="leagues__icon" />
+        <img src={delete_icon} alt="delete icon" className="leagues__icon" />
+      </div>
       <div className="leagues__img"></div>
       <div className="leagues__container">
-        <p className="leagues_date">Created: 20/3/24</p>
-        <p className="leagues_teams">Teams: 10</p>
-        <div className="leagues__buttons">
-          <img src={edit_icon} alt="edit icon" className="leagues__icon" />
-          <img src={delete_icon} alt="delete icon" className="leagues__icon" />
-        </div>
+        <p className="leagues_date">
+          Created: {new Date(date).toLocaleDateString()}
+        </p>
+        <p className="leagues_teams">Teams: {teamCount}</p>
       </div>
     </article>
   );
--- src/pages/myLeagues/myLeagues.jsx
+++ src/pages/myLeagues/myLeagues.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import LeagueCard from "../../components/leagueCard/leagueCard";
 import "./myLeagues.scss";
+import axios from "axios";
 
 export default function MyLeagues() {
+  // const apiURL = process.env.REACT_APP_API_URL;
+  const apiURL = `http://localhost:5050`;
+  const user_id = 1;
+
+  const [allLeagues, setAllLeagues] = useState(null);
+
+  const getLeagues = async () => {
+    try {
+      const { data } = await axios.get(`${apiURL}/api/leagues/${user_id}`);
+      setAllLeagues(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLeagues();
+  }, []);
+
   return (
     <div className="myLeagues">
       <section className="subheader">
@@ -9,8 +30,17 @@ export default function MyLeagues() {
         <button className="subheader__btn">+ Add League</button>
       </section>
       <section className="leagues">
-        <LeagueCard />
-        <LeagueCard />
+        {allLeagues &&
+          allLeagues.map((league) => {
+            return (
+              <LeagueCard
+                key={league.id}
+                id={league.id}
+                name={league.name}
+                date={league.create_at}
+              />
+            );
+          })}
       </section>
     </div>
   );
